9e5c5241a35156520aee48f7d00d3841868dfe75
Mylene Febrissy <m.febrissy@gmail.com>
2024-03-20 23:42:43 +0100

    Created OrderProducts model/controller/views, added log out button to admin nav

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,11 @@
 class Product < ApplicationRecord
-  has_many :stocks
-  belongs_to :category
-
   has_many_attached :images do |attachable|
     attachable.variant :thumb, resize_to_limit: [100, 100]
     attachable.variant :medium, resize_to_limit: [250, 250]
   end
+  
+  has_many :stocks
+  has_many :order_products
+  belongs_to :category
+
 end
