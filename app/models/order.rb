9c540c4cff9f0c78bf3363244a5604bdfa6c3d84
Mylene Febrissy <m.febrissy@gmail.com>
2024-03-21 13:43:58 +0100

    Added associations between Order and Product through :order_products, implemented AdminsController#home for stats, created Admins home view

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
 class Order < ApplicationRecord
   has_many :order_products
+  has_many :products, through: :order_products
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,10 @@ class Product < ApplicationRecord
     attachable.variant :thumb, resize_to_limit: [100, 100]
     attachable.variant :medium, resize_to_limit: [250, 250]
   end
-  
+
   has_many :stocks
   has_many :order_products
+  has_many :orders, through: :order_products
   belongs_to :category
 
 end
